9b494996802edc0862cd9095a59ed6cdd5a7474c
yu40ta <yu40ta.engineer@gmail.com>
2022-01-30 16:32:22 +0900

    add: TODO 用ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'todos#index'
+  get 'todos/show'
+  get 'todos/new'
+  get 'todos/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
