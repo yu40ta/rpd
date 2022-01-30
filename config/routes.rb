2ba87dc6fef5bf172ca6def9c8e803c218249ad1
yu40ta <yu40ta.engineer@gmail.com>
2022-01-30 19:40:27 +0900

    update: ルーティング一括設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'todos#index'
-  get 'todos/show'
-  get 'todos/new'
-  get 'todos/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'todos#index'
+  resources :todos
 end
