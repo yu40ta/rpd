2a02c8272158b4e9a9d454ea8dea96b6a3518fb0
yu40ta <yu40ta.engineer@gmail.com>
2022-02-20 23:58:09 +0900

    update: Bootstrap の一時無効化

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,12 +3,12 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 // jQuery と Bootstrap の JS を扱えるようにする
-const webpack = require('webpack')
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
-  })
-) 
+// const webpack = require('webpack')
+// environment.plugins.append(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//     Popper: ['popper.js', 'default']
+//   })
+// ) 
